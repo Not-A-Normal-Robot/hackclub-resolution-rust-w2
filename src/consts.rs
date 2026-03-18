eecc1ffdcf30e1e00537a5e4c42322644d73b18a
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-03-18 23:22:10 +0700

    Do some refactoring

--- src/consts.rs
+++ src/consts.rs
@@ -4,3 +4,6 @@ pub static APP_NAME: &str = "20³";
 
 pub static TEXT_UNTIL_NEXT: &str = "until next interruption";
 pub static TEXT_LOOK_FAR: &str = "Look at something 20 ft (6 m) away";
+
+pub const DEFAULT_DELAY: Duration = Duration::from_secs(6);
+pub const DEFAULT_LENGTH: Duration = Duration::from_secs(3);
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@ use std::time::SystemTime;
 
 use eframe::{
     App, NativeOptions,
-    egui::{self, Color32, RichText, Ui, Vec2, ViewportBuilder},
+    egui::{self, Color32, ProgressBar, RichText, Ui, Vec2, ViewportBuilder},
 };
 
-use crate::consts::TEXT_UNTIL_NEXT;
+use crate::consts::{DEFAULT_DELAY, DEFAULT_LENGTH, TEXT_UNTIL_NEXT};
 
 pub mod consts;
 
@@ -21,8 +21,8 @@ impl TwentyCubedApp {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self {
             last_trigger: SystemTime::now(),
-            delay: Duration::from_mins(20),
-            length: Duration::from_secs(20),
+            delay: DEFAULT_DELAY,
+            length: DEFAULT_LENGTH,
         }
     }
 }
@@ -35,36 +35,51 @@ impl App for TwentyCubedApp {
                 .duration_since(SystemTime::now())
                 .unwrap_or_default();
             main_duration(duration, ui);
+            duration_progress(duration, self.delay, ui);
         });
 
-        ctx.request_repaint_after(Duration::from_secs(1));
+        ctx.request_repaint_after(Duration::from_millis(250));
     }
+}
+
+fn format_duration(duration: Duration) -> String {
+    let h = duration.div_duration_f64(Duration::from_hours(1)).floor();
+    let rem = duration
+        .checked_sub(Duration::from_hours(1).mul_f64(h))
+        .unwrap();
+    let m = rem.div_duration_f64(Duration::from_mins(1)).floor();
+    let rem = rem.checked_sub(Duration::from_mins(1).mul_f64(m)).unwrap();
+    let s = rem.as_secs_f64().floor();
+
+    let mut string = String::with_capacity(2 + 1 + 2 + 1 + 2);
+
+    if h > 0.0 {
+        write!(string, "{h:02}:").expect("formatting and writing should work");
+    }
+
+    write!(string, "{m:02}:{s:02}").expect("formatting and writing should work");
+
+    string
 }
 
 fn main_duration(duration: Duration, ui: &mut Ui) {
     ui.centered_and_justified(|ui| {
-        let h = duration.div_duration_f64(Duration::from_hours(1)).floor();
-        let rem = duration
-            .checked_sub(Duration::from_hours(1).mul_f64(h))
-            .unwrap();
-        let m = rem.div_duration_f64(Duration::from_mins(1)).floor();
-        let rem = rem.checked_sub(Duration::from_mins(1).mul_f64(m)).unwrap();
-        let s = rem.as_secs_f64().floor();
-
-        let mut string = String::with_capacity(2 + 1 + 2 + 1 + 2);
-
-        if h > 0.0 {
-            write!(string, "{h:02}:").expect("formatting and writing should work");
-        }
-
-        write!(string, "{m:02}:{s:02}").expect("formatting and writing should work");
-
+        let string = format_duration(duration);
         #[expect(clippy::cast_precision_loss)]
         let font_size = ui.available_width() / string.len() as f32;
         let font_size = font_size.min(ui.available_height() / 2.0);
 
         ui.label(RichText::new(string).color(Color32::WHITE).size(font_size));
     });
+}
+
+fn duration_progress(duration: Duration, total: Duration, ui: &mut Ui) {
+    let duration = Duration::from_secs(duration.as_secs());
+    ui.add(
+        ProgressBar::new(duration.div_duration_f32(total))
+            .fill(Color32::WHITE)
+            .corner_radius(0),
+    );
 }
 
 #[must_use]
